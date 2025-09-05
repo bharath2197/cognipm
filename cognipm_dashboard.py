588d15052fd6e656b40d18ed6b656cf5499733c4
bharath2197 <bharathsrini33@gmail.com>
2025-09-05 11:16:31 -0400

    Update cognipm_dashboard.py

--- cognipm_dashboard.py
+++ cognipm_dashboard.py
@@ -2,6 +2,7 @@ import sys
 import os
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 import streamlit as st
+st.title("Cognipm is loading...")
 import requests
 import io
 from product_agent import run_product_agent
